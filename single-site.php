8946bced390136fd9a3b55b70beecbd26c3b60b3
Adrian Short <adrian@adrianshort.org>
2015-03-24 09:45:19 +0000

    Let's go

---
+++ single-site.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		<article class="post">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			/*
+			 * Include the post format-specific template for the content. If you want to
+			 * use this in a child theme, then include a file called called content-___.php
+			 * (where ___ is the post format) and that will be used instead.
+			 */
+			// get_template_part( 'content', get_post_format() );
+			
+			
+			echo "<h1>" . get_the_title() . "</h1>";
+			talhyperlocal_map_shortcode( get_the_ID() );
+			the_content();
+			the_meta();
+			$meta = get_post_custom();
+// 			print_r($meta);
+			
+			$url = $meta['url'][0];
+			?>
+			<p><a href="<?php echo $url ?>">Website</a></p>
+			<p><?php the_taxonomies( 'before=<ul>&after=</ul>' ) ?></p>
+			<?php			
+			// If comments are open or we have at least one comment, load up the comment template.
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
+
+			// Previous/next post navigation.
+			// the_post_navigation( array(
+			// 	'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
+			// 		'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
+			// 		'<span class="post-title">%title</span>',
+			// 	'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+			// 		'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
+			// 		'<span class="post-title">%title</span>',
+			// ) );
+
+		// End the loop.
+		endwhile;
+		?>
+			</article>
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
