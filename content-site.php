afcc8f534599855ab8d401eaf6a64096904feac6
Adrian Short <adrian@adrianshort.org>
2015-04-13 17:33:46 +0100

    Fix layout

---
+++ content-site.php
@@ -0,0 +1,57 @@
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <?php
+    // Post thumbnail.
+    twentyfifteen_post_thumbnail();
+  ?>
+
+  <header class="entry-header">
+    <?php
+      if ( is_single() ) :
+        the_title( '<h1 class="entry-title">', '</h1>' );
+      else :
+        the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+      endif;
+    ?>
+  </header><!-- .entry-header -->
+
+  <div class="entry-content">
+    <?php
+      /* translators: %s: Name of current post */
+      the_content( sprintf(
+        __( 'Continue reading %s', 'twentyfifteen' ),
+        the_title( '<span class="screen-reader-text">', '</span>', false )
+      ) );
+
+      talhyperlocal_map_shortcode( get_the_ID() );
+      $meta = get_post_custom();
+      $url = $meta['url'][0];
+      ?>
+      <p>Website: <a href="<?php echo $url ?>"><?php echo $url ?></a></p>
+      <p><?php the_taxonomies( 'before=<ul>&after=</ul>' ) ?></p>
+
+
+<!--       wp_link_pages( array(
+        'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+        'after'       => '</div>',
+        'link_before' => '<span>',
+        'link_after'  => '</span>',
+        'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
+        'separator'   => '<span class="screen-reader-text">, </span>',
+      ) );
+ -->
+  </div><!-- .entry-content -->
+
+  <?php
+    // Author bio.
+    if ( is_single() && get_the_author_meta( 'description' ) ) :
+      get_template_part( 'author-bio' );
+    endif;
+  ?>
+
+  <footer class="entry-footer">
+    <?php twentyfifteen_entry_meta(); ?>
+    <?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
+  </footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
--- single-site.php
+++ single-site.php
@@ -21,30 +21,22 @@ get_header(); ?>
 			 * use this in a child theme, then include a file called called content-___.php
 			 * (where ___ is the post format) and that will be used instead.
 			 */
-			get_template_part( 'content', get_post_format() );
+			get_template_part( 'content', 'site' );			
 			
-			
-			talhyperlocal_map_shortcode( get_the_ID() );
-			$meta = get_post_custom();
-			$url = $meta['url'][0];
-			?>
-			<p><a href="<?php echo $url ?>">Website</a></p>
-			<p><?php the_taxonomies( 'before=<ul>&after=</ul>' ) ?></p>
-			<?php			
 			// If comments are open or we have at least one comment, load up the comment template.
 			// if ( comments_open() || get_comments_number() ) :
 			// 	comments_template();
 			// endif;
 
 			// Previous/next post navigation.
-			// the_post_navigation( array(
-			// 	'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-			// 		'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-			// 		'<span class="post-title">%title</span>',
-			// 	'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-			// 		'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-			// 		'<span class="post-title">%title</span>',
-			// ) );
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+			) );
 
 		// End the loop.
 		endwhile;
