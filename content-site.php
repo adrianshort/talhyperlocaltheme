3abe7982b06a8fc355053781e03716bd9375641c
Adrian Short <adrian@adrianshort.org>
2015-04-13 17:43:19 +0100

    Remove author bio

--- content-site.php
+++ content-site.php
@@ -44,9 +44,9 @@
 
   <?php
     // Author bio.
-    if ( is_single() && get_the_author_meta( 'description' ) ) :
-      get_template_part( 'author-bio' );
-    endif;
+    // if ( is_single() && get_the_author_meta( 'description' ) ) :
+    //   get_template_part( 'author-bio' );
+    // endif;
   ?>
 
   <footer class="entry-footer">
