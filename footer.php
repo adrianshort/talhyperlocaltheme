a99f4ba9ac68b4425920cc9ddf3658b46b372683
Mike Rawlins <michaeljrawlins@Mikes-iMac-2.local>
2015-05-13 15:53:28 +0100

    Googles
    Add the googles tracking code.
    
    All-bar the actual tracking number, you need to add that in

--- footer.php
+++ footer.php
@@ -27,7 +27,16 @@
 	</footer><!-- .site-footer -->
 
 </div><!-- .site -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-add-your-tracking-number-here', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <?php wp_footer(); ?>
 
 </body>
